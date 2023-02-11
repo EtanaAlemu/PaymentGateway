fb51b489ed3267d7b078cdfe48a256eb6e7ab204
EtanaAlemu <etanaalemunew@gmail.com>
2023-02-11 10:55:43 +0300

    first commit

---
+++ app/src/main/java/com/dxvalley/paymentgateway/ui/PaymentActivity.java
@@ -0,0 +1,13 @@
+package com.dxvalley.paymentgateway.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
